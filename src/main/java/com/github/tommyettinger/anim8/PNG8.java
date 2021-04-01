3c08cb1bd1bc7248dd681ec8b20dd255a59a6f28
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-03-31 20:26:25 -0700

    Update AnimatedGif, PNG8, images.

--- src/main/java/com/github/tommyettinger/anim8/PNG8.java
+++ src/main/java/com/github/tommyettinger/anim8/PNG8.java
@@ -1308,10 +1308,11 @@ public class PNG8 implements AnimationWriter, Dithered, Disposable {
             buffer.writeInt(IDAT);
             deflater.reset();
 
-            int color, used, rdiff, gdiff, bdiff;
+            int color, used;
+            float rdiff, gdiff, bdiff;
             float er, eg, eb;
             byte paletteIndex;
-            float w1 = (float)(palette.ditherStrength * palette.populationBias * 0.15625), w3 = w1 * 3f, w5 = w1 * 5f, w7 = w1 * 7f;
+            float w1 = (float)(palette.ditherStrength * palette.populationBias * 4.0), w3 = w1 * 3f, w5 = w1 * 5f, w7 = w1 * 7f;
 
             byte[] lineOut, curLine, prevLine;
             if (lineOutBytes == null) {
@@ -1358,9 +1359,9 @@ public class PNG8 implements AnimationWriter, Dithered, Disposable {
                                         | ((gg << 2) & 0x3E0)
                                         | ((bb >>> 3))];
                         used = paletteArray[paletteIndex & 0xFF];
-                        rdiff = (color>>>24)-    (used>>>24);
-                        gdiff = (color>>>16&255)-(used>>>16&255);
-                        bdiff = (color>>>8&255)- (used>>>8&255);
+                        rdiff = OtherMath.cbrtShape(0x1.Cp-8f * ((color>>>24)-    (used>>>24))    );
+                        gdiff = OtherMath.cbrtShape(0x1.Cp-8f * ((color>>>16&255)-(used>>>16&255)));
+                        bdiff = OtherMath.cbrtShape(0x1.Cp-8f * ((color>>>8&255)- (used>>>8&255)) );
                         if(px < w - 1)
                         {
                             curErrorRed[px+1]   += rdiff * w7;
@@ -1620,10 +1621,11 @@ public class PNG8 implements AnimationWriter, Dithered, Disposable {
             buffer.writeInt(IDAT);
             deflater.reset();
 
-            int color, used, rdiff, gdiff, bdiff;
+            int color, used;
+            float rdiff, gdiff, bdiff;
             float er, eg, eb;
             byte paletteIndex;
-            float w1 = (float)(palette.ditherStrength * palette.populationBias * 0.15625), w3 = w1 * 3f, w5 = w1 * 5f, w7 = w1 * 7f;
+            float w1 = (float)(palette.ditherStrength * palette.populationBias * 4.0), w3 = w1 * 3f, w5 = w1 * 5f, w7 = w1 * 7f;
 
             byte[] lineOut, curLine, prevLine;
             if (lineOutBytes == null) {
@@ -1671,9 +1673,9 @@ public class PNG8 implements AnimationWriter, Dithered, Disposable {
                                         | ((gg << 2) & 0x3E0)
                                         | ((bb >>> 3))];
                         used = paletteArray[paletteIndex & 0xFF];
-                        rdiff = (color>>>24)-    (used>>>24);
-                        gdiff = (color>>>16&255)-(used>>>16&255);
-                        bdiff = (color>>>8&255)- (used>>>8&255);
+                        rdiff = OtherMath.cbrtShape(0x1.Cp-8f * ((color>>>24)-    (used>>>24))    );
+                        gdiff = OtherMath.cbrtShape(0x1.Cp-8f * ((color>>>16&255)-(used>>>16&255)));
+                        bdiff = OtherMath.cbrtShape(0x1.Cp-8f * ((color>>>8&255)- (used>>>8&255)) );
                         if(px < w - 1)
                         {
                             curErrorRed[px+1]   += rdiff * w7;
@@ -2532,10 +2534,11 @@ public class PNG8 implements AnimationWriter, Dithered, Disposable {
 
             lastLineLen = w;
 
-            int color, used, rdiff, gdiff, bdiff;
+            int color, used;
+            float rdiff, gdiff, bdiff;
             float er, eg, eb;
             byte paletteIndex;
-            float w1 = (float)(palette.ditherStrength * palette.populationBias * 0.15625), w3 = w1 * 3f, w5 = w1 * 5f, w7 = w1 * 7f;
+            float w1 = (float)(palette.ditherStrength * palette.populationBias * 4.0), w3 = w1 * 3f, w5 = w1 * 5f, w7 = w1 * 7f;
 
             int seq = 0;
             for (int i = 0; i < frames.size; i++) {
@@ -2606,9 +2609,9 @@ public class PNG8 implements AnimationWriter, Dithered, Disposable {
                                             | ((gg << 2) & 0x3E0)
                                             | ((bb >>> 3))];
                             used = paletteArray[paletteIndex & 0xFF];
-                            rdiff = (color>>>24)-    (used>>>24);
-                            gdiff = (color>>>16&255)-(used>>>16&255);
-                            bdiff = (color>>>8&255)- (used>>>8&255);
+                            rdiff = OtherMath.cbrtShape(0x1.Cp-8f * ((color>>>24)-    (used>>>24))    );
+                            gdiff = OtherMath.cbrtShape(0x1.Cp-8f * ((color>>>16&255)-(used>>>16&255)));
+                            bdiff = OtherMath.cbrtShape(0x1.Cp-8f * ((color>>>8&255)- (used>>>8&255)) );
                             if(px < w - 1)
                             {
                                 curErrorRed[px+1]   += rdiff * w7;
@@ -2905,10 +2908,11 @@ public class PNG8 implements AnimationWriter, Dithered, Disposable {
 
             lastLineLen = w;
 
-            int color, used, rdiff, gdiff, bdiff;
+            int color, used;
+            float rdiff, gdiff, bdiff;
             float er, eg, eb;
             byte paletteIndex;
-            float w1 = (float)(palette.ditherStrength * palette.populationBias * 0.15625), w3 = w1 * 3f, w5 = w1 * 5f, w7 = w1 * 7f;
+            float w1 = (float)(palette.ditherStrength * palette.populationBias * 4.0), w3 = w1 * 3f, w5 = w1 * 5f, w7 = w1 * 7f;
 
             int seq = 0;
             for (int i = 0; i < frames.size; i++) {
@@ -2980,9 +2984,9 @@ public class PNG8 implements AnimationWriter, Dithered, Disposable {
                                             | ((gg << 2) & 0x3E0)
                                             | ((bb >>> 3))];
                             used = paletteArray[paletteIndex & 0xFF];
-                            rdiff = (color>>>24)-    (used>>>24);
-                            gdiff = (color>>>16&255)-(used>>>16&255);
-                            bdiff = (color>>>8&255)- (used>>>8&255);
+                            rdiff = OtherMath.cbrtShape(0x1.Cp-8f * ((color>>>24)-    (used>>>24))    );
+                            gdiff = OtherMath.cbrtShape(0x1.Cp-8f * ((color>>>16&255)-(used>>>16&255)));
+                            bdiff = OtherMath.cbrtShape(0x1.Cp-8f * ((color>>>8&255)- (used>>>8&255)) );
                             if(px < w - 1)
                             {
                                 curErrorRed[px+1]   += rdiff * w7;
